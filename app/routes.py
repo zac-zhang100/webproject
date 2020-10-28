518bb88521a8e742011dbea4724b61d28d74a064
zac zhang <zac-zhang@outlook.com>
2020-10-28 16:19:35 +0800

    create some test demo and set routes' configuration to visit the pages

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template
+from flask import render_template,flash,redirect,url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
-@app.route('/index')
+@app.route('/base')
 def index():
     name = 'Zac Zhang'
     movies = [
@@ -17,4 +18,13 @@ def index():
         {'title': 'WALL-E', 'year': '2008'},
         {'title': 'The	Pork	of	Music', 'year': '2012'},
     ]
-    return render_template('index.html', name=name, movies=movies)
+    return render_template('base.html', name=name, movies=movies)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data,form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html',title='Sign In',form=form)
--- colors.py
+++ colors.py
@@ -1,64 +1,48 @@
 from flask import Flask, jsonify
 from flasgger import Swagger
 from app import app
+
 swagger = Swagger(app)
 
-@app.route('/player/add/')
-def add():
+@app.route('/colors/<palette>/')
+def colors(palette):
     """Example endpoint returning a list of colors by palette
-    方法名称:增加选手
-    方法描述：调用此api增加一名选手
+    This is using docstrings for specifications.
     ---
-    tags：
-      -接口名称：选手管理API
     parameters:
-      - name: body
-        in: body
-        required：true
-        schema:
-          id: player
-          required:
-            - nickname
-            - name
-            - phone
-          properties:
-            nickname:
-              type: string
-              description: 昵称
-              default: "XXX"
-            name:
-              type: string
-              description: 姓名
-              default: "XXX"
-            nickname:
-              type: string
-              description: 电话
-              default: "XXX"
+      - name: palette
+        in: path
+        type: string
+        enum: ['all', 'rgb', 'cmyk']
+        required: true
+        default: all
+    definitions:
+      Palette:
+        type: object
+        properties:
+          palette_name:
+            type: array
+            items:
+              $ref: '#/definitions/Color'
+      Color:
+        type: string
     responses:
       200:
-        description: 状态描述：成功返回json格式
+        description: A list of colors (may be filtered by palette)
         schema:
-          id: Success
-          properties:
-            code:
-              type: string
-              description: 状态码
-              default: 200
-            msg:
-              type: string
-              description：信息
-              default: "ok"
-            error_code:
-              type: string
-              description：错误码
-              default: o
+          $ref: '#/definitions/Palette'
+        examples:
+          rgb: ['red', 'green', 'blue']
+    """
+    all_colors = {
+        'cmyk': ['cian', 'magenta', 'yellow', 'black'],
+        'rgb': ['red', 'green', 'blue']
+    }
+    if palette == 'all':
+        result = all_colors
+    else:
+        result = {palette: all_colors.get(palette)}
 
-    player = {
-      "name" : "zac",
-      "number" : "123456",
-      "nickname" : "zac"
-      }
-
-    return jsonify(player)
+    return jsonify(result)
 
 app.run(debug=True)
